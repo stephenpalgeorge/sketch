ddffe5cc9077f2c8946b974ee7e05199be2cf81c
Stephen Pal-George <stephenpalgeorge@gmail.com>
2024-05-18 20:30:48 +0100

    initial commit for page setup

---
+++ src/routes/projects/mouse-follow/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { Canvas, Circle, Scene } from "$lib";
+
+    let canvas: Canvas;
+    let scene: Scene;
+
+    function setup(): void {
+        canvas = new Canvas('#random-walker-canvas');
+        if (!canvas.context) return;
+
+        scene = new Scene(canvas.element);
+
+        const mover = new Circle('first-walker', canvas.context, 400, 400, 16, '#000000');
+        scene.add(mover);
+
+        requestAnimationFrame(draw);
+    }
+
+    function draw(): void {
+        canvas.background('#fff');
+        scene.render();
+        requestAnimationFrame(draw);
+    }
+
+    onMount(() => setup());
+</script>
+
+<canvas id="random-walker-canvas" width="800" height="800">
+    Canvas not supported
+</canvas>
